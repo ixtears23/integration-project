e96c2174eccdbf7f9985188ea91f736849bd3362
JunseokOh <ixtears23@icloud.com>
2024-04-24 08:37:08 +0900

    Virtual Thread와 Platform Thread 생성 코드 추가

--- virtual-thread/src/main/java/junseok/snr/virtualthread/demo/VirtualThreadDemo.java
+++ virtual-thread/src/main/java/junseok/snr/virtualthread/demo/VirtualThreadDemo.java
@@ -1,4 +1,16 @@
 package junseok.snr.virtualthread.demo;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class VirtualThreadDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.ofVirtual().start(() -> {
+            log.info("=== virtualThread : {}", Thread.currentThread());
+        });
+
+        Thread.ofPlatform().start(() -> {
+            log.info("=== platform Thread : {}", Thread.currentThread());
+        });
+    }
 }
